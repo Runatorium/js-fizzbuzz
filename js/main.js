8ff6dc930a4cbde250b5b19be98be6bb87a46bad
Runatorium <stefano.alesci1@gmail.com>
2023-02-24 15:31:50 +0100

    graphic added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+const Domcontainer = document.querySelector('.container');
+
 for (let i = 1; i <= 100; i++){
-    if (i % 3 === 0){
-        console.log('fizz');
-    }
-    if (i % 5 === 0){
-        console.log('buzz');
-    }
     if (i % 3 ===0 && i % 5 === 0){
         console.log('Fizzbuzz')
+        const square = `<div class="square fizzbuzz">'Fizzbuzz'</div>`;
+        Domcontainer.innerHTML += square;
+    }
+    else if (i % 5 === 0){
+        console.log('buzz');
+        const square = `<div class="square buzz">'buzz'</div>`;
+        Domcontainer.innerHTML += square;
+    }
+    else if (i % 3 === 0){
+        console.log('fizz');
+        const square = `<div class="square fizz">'Fizz'</div>`;
+        Domcontainer.innerHTML += square;
     }
     else {
         console.log(i);
+        const square = `<div class="square">'${i}'</div>`;
+        Domcontainer.innerHTML += square;
     }
 }
+
+
