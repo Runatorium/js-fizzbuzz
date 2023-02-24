bdf12c26b6ae4f2fca98b425b0575bb99a3f01ff
Runatorium <stefano.alesci1@gmail.com>
2023-02-24 16:12:31 +0100

    Ottimizzazione css

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ for (let i = 1; i <= 100; i++){
     if (i % 3 ===0 && i % 5 === 0){
         console.log('Fizzbuzz')
         const square = document.createElement('div');
-        square.classList.add('square-fizzbuzz');
+        square.classList.add('fizzbuzz');
         square.append('fizzbuzz');
         Domcontainer.append(square);;
     }
     else if (i % 5 === 0){
         console.log('buzz');
         const square = document.createElement('div');
-        square.classList.add('square-buzz');
+        square.classList.add('buzz');
         square.append('buzz');
         Domcontainer.append(square);
     }
     else if (i % 3 === 0){
         console.log('fizz');
         const square = document.createElement('div');
-        square.classList.add('square-fizz');
+        square.classList.add('fizz');
         square.append('fizz');
         Domcontainer.append(square);
     }
@@ -32,10 +32,3 @@ for (let i = 1; i <= 100; i++){
     }
 }
 
-
-
-
-
-
-        //const square = `<div class="square">'${i}'</div>`;
-        //Domcontainer.innerHTML += square;
