68738a3c344bfd5d6f245697af32e3f94130e009
Runatorium <stefano.alesci1@gmail.com>
2023-02-24 16:05:01 +0100

    Bonus cambio di metodo da .Innerhtml a .append()

--- js/main.js
+++ js/main.js
@@ -3,24 +3,39 @@ const Domcontainer = document.querySelector('.container');
 for (let i = 1; i <= 100; i++){
     if (i % 3 ===0 && i % 5 === 0){
         console.log('Fizzbuzz')
-        const square = `<div class="square fizzbuzz">'Fizzbuzz'</div>`;
-        Domcontainer.innerHTML += square;
+        const square = document.createElement('div');
+        square.classList.add('square-fizzbuzz');
+        square.append('fizzbuzz');
+        Domcontainer.append(square);;
     }
     else if (i % 5 === 0){
         console.log('buzz');
-        const square = `<div class="square buzz">'buzz'</div>`;
-        Domcontainer.innerHTML += square;
+        const square = document.createElement('div');
+        square.classList.add('square-buzz');
+        square.append('buzz');
+        Domcontainer.append(square);
     }
     else if (i % 3 === 0){
         console.log('fizz');
-        const square = `<div class="square fizz">'Fizz'</div>`;
-        Domcontainer.innerHTML += square;
+        const square = document.createElement('div');
+        square.classList.add('square-fizz');
+        square.append('fizz');
+        Domcontainer.append(square);
     }
     else {
         console.log(i);
-        const square = `<div class="square">'${i}'</div>`;
-        Domcontainer.innerHTML += square;
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.append(i);
+        Domcontainer.append(square);
+        
     }
 }
 
 
+
+
+
+
+        //const square = `<div class="square">'${i}'</div>`;
+        //Domcontainer.innerHTML += square;
